f3c2a8828ca2485f7eb36817fc8b74663b3deb12
Walker D. Adams <Tsolval@gmail.com>
2018-12-09 00:58:13 -0600

    database updates are now stored in git

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,7 @@
-insert into page (title, subject, body, author, date ) values ('gettingStarted', 'Getting Started', 'Page Creation: To create a new page, click the plus sign in the navigation menu and fill out the form.  \nPage Navigation: To navigate to an existing page, add the title to the url or search for the page.', 'pwiki', '2017-08-01');
+insert into page (title, body, author, date ) values ('Home', '## Welcome to Portable Wiki!
+### Create Page: 
+To create a new page, click the "Add Page" button in the navigation menu.
+### Update Page:
+To update a page, click the "Edit Page" button in the navigation menu.
+### Show Page: 
+To navigate to an existing page, add the title to the URL or search for the page.', 'pwiki', '2017-08-01');
